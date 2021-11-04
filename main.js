bf75667c64b0b6e516f5cbae36f0a5ce43b339b3
xiebian <xiebianwhu@gmail.com>
2021-11-04 23:23:07 +0800

    use component implementation

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const app = Vue.createApp({
         return {
             message: "hello world",
             tasks: ["pick up kids", "buy some food"],
-            task: null,
+            // task: null,
         }
     },
 
@@ -14,11 +14,49 @@ const app = Vue.createApp({
     },
 
     methods: {
-        onEnter() {
-            this.tasks.push(this.task)
-            this.task = null
+        addNewTask(task) {
+            this.tasks.push(task)
+            // this.task = null
         }
     }
+})
+
+app.component('taskcompound', {
+    emits: ['add_task'], 
+    props: {
+        tasks: {
+            type: Object,
+            required: true,
+        }
+    },
+    data() {
+        return {
+            task: null,
+        }
+    },
+    template: `
+    <input type="text" v-model="task" @keyup.enter="onEnter">
+    <tasklist :tasks="tasks"></tasklist>
+    `,
+    methods: {
+        onEnter() {
+            this.$emit('add_task',this.task)
+        }
+    }
+})
+
+app.component('tasklist', {
+    props: {
+        tasks: {
+            type: Object,
+            required: true,
+        }
+    },
+    template: `
+    <div class="round_corner" v-for="(ltask,id) in tasks" :key="id">
+    <p>{{ltask}}</p>
+    </div>
+    `
 })
 
 const appMount = app.mount("#app")
